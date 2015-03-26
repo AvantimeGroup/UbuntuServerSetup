2724c5888d46b4fb2cff7cecb02cce9ddeb6fbff
Jens Wennberg <jens.wennberg@avantime.se>
2015-03-26 12:31:47 +0100

    bug fix in mysql_config

--- mysql_config.sh
+++ mysql_config.sh
@@ -8,8 +8,9 @@ Q1="CREATE DATABASE IF NOT EXISTS $1;"
 Q2="GRANT USAGE ON *.* TO $2@localhost IDENTIFIED BY '$3';"
 Q3="GRANT ALL PRIVILEGES ON $1.* TO $2@localhost;"
 Q4="FLUSH PRIVILEGES;"
-Q5="source $4;"
-SQL="${Q1}${Q2}${Q3}${Q4}${Q5}"
+Q5="use $1;"
+Q6="source $4;"
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
   
 if [ $# -ne $EXPECTED_ARGS ]
 then
