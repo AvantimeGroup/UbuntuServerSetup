0eb57eb9f7f49049e8d577fbf55f7cc258cfd194
Jens Wennberg <jens.wennberg@avantime.se>
2015-03-26 08:21:33 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Get the current directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "** installing packages"
+echo ""
+apt-get install fail2ban incron clamav
+echo "** configuring fail2ban"
+echo ""
+cd /etc/fail2ban/filter.d/
+wget http://plugins.svn.wordpress.org/wp-fail2ban/trunk/wordpress.conf
+cd $DIR
+cp fail2ban/wordpress.conf /etc/fail2ban/jail.d/
+cp fail2ban/jail.local /etc/fail2ban/
+
+echo "** Configuring virus scan on upload folders"
+find /var/www/ -name "uploads" -type d -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE,IN_CREATE,IN_DELETE clamscan -r --remove {}" > /etc/incron.d/webroot.conf
