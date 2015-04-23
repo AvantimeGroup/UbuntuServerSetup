fc1d6fbf0c108eea936028d091f83bd8349d7ea5
Erik Johansson <erik.johansson@avantime.se>
2015-04-23 08:18:55 +0200

    fixed a bug where the underlaying config files never was printed into the main config

--- monit.sh
+++ monit.sh
@@ -38,7 +38,7 @@ echo "<Location /server-status>
  </Location>" >> /etc/apache2/apache2.conf
 
 /etc/init.d/apache2 restart
- 
+
 
 echo "set alert $email" >> $CONFIG
 echo "set mailserver $mailserver"
@@ -51,32 +51,32 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
    echo "##############################################
-#####        $f                   ##### 
+#####        $f                   #####
 ##############################################" >> $CONFIG
   cat $f >> $CONFIG
-  
+
   if [[ $f == *"apache2" ]]
     then
-    awk '$1~/^ServerName/{print $2}' /etc/apache2/sites-available/* | while read -r host ; do  
+    awk '$1~/^ServerName/{print $2}' /etc/apache2/sites-available/* | while read -r host ; do
     echo "check host $host with address $host
       if failed
-         port 80 protocol http         
-         request /?monit=1 with content = 'Monit [0-9.]+'        
+         port 80 protocol http
+         request /?monit=1 with content = 'Monit [0-9.]+'
       then alert" >> $CONFIG
     done
-    
+
   fi;
-  
-else 
+
+else
    echo "skiping $f"
 fi
- 
+
  # do something on $f
-done 
+done
 
 for f in monit_config/config*
 do
-  echo >> $CONFIG
+  echo f >> $CONFIG
 done
 
 chmod 700 $CONFIG
